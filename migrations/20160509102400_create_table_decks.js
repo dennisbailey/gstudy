061779b1b6e0852e19fedfdc0a0bb9e02c843b2c
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-14 11:50:55 -0600

    Added an image column to the decks table

--- migrations/20160509102400_create_table_decks.js
+++ migrations/20160509102400_create_table_decks.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
     table.integer('creator_id').references('users', 'id');
     table.string('name');
     table.string('desc');
+    table.string('img');
     table.timestamp('dt_created').notNullable().defaultTo(knex.fn.now());
     table.timestamp('dt_modified').notNullable().defaultTo(knex.fn.now());
   });
